2dbe0ed49cdda86a08fe655f1b212bd289bf5c91
AngelWingJasmineT <kterada0322@gmail.com>
2019-12-25 18:29:01 +0900

    初期準備完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -8,4 +9,11 @@ class ApplicationController < ActionController::Base
       username == 'mercari' && password == '1111'
     end
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :age, :family_name, :first_name, :family_name_kana, :first_name_kana ,:year ,:month ,:day])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
   root to: 'top#index'
   resources :users, only: [:index, :new, :registration]
 end
