81516ee30155de4cf26495dd518791cf4dd9431e
sorakun1112 <elope_with_dolly1112@yahoo.co.jp>
2019-12-25 14:48:44 +0900

    item_details_view

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,6 @@
+$(function(){
+  $(".thumbnails-frame__thumbnail img").on('mouseover',function(){
+    var dataUrl = $(this).attr('data-url');
+    $("#main-image img").attr('src',dataUrl);
+  });
+});
