10c52d33b2e8907111021b14e202b9746ee4de32
sorakun1112 <elope_with_dolly1112@yahoo.co.jp>
2019-12-25 17:40:34 +0900

    fixed

---
+++ app/assets/javascripts/top.js
@@ -0,0 +1,11 @@
+$(function(){
+  $('.picture1__scroll').slick({
+  dots: true,
+  arrows: true,
+  accessibility: true,
+  // prevArrow:'<div class="prev">←</div>',
+  // nextArrow:'<div class="next">NEXT</div>',
+  autoplay: true,
+  autoplayspeed: 5000,
+  });
+});
