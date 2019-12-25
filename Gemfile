cbd0f65108096dd3baf0139801cb3fb9e5588cd6
AngelWingJasmineT <kterada0322@gmail.com>
2019-12-25 14:05:57 +0900

    user-new-registration

--- Gemfile
+++ Gemfile
@@ -77,3 +77,5 @@ gem 'haml-rails'
 gem 'erb2haml'
 gem 'font-awesome-sass'
 gem 'carrierwave'
+
+gem 'jquery-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'testtop#index'
+  resources :users, only: [:index, :new, :registration]
 end
