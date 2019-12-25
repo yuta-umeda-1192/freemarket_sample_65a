315305fae21158b335326ee9f7b2f349d212bc96
GORI16 <ohnoyusuke16@icloude.com>
2019-12-25 20:46:08 +0900

    edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'top#index'
-  resources :item_exhibit, only: [:index, :create]
+  resources :items, only: [:new, :create]
   resources :users, only: [:index, :new, :registration]
 end
