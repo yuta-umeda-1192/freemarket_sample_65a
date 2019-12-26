51a89ab95cf7a45b6a8e8def05de52b5dab9dbbb
GORI16 <ohnoyusuke16@icloude.com>
2019-12-26 19:46:57 +0900

    edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,16 @@
 class ItemsController < ApplicationController
 
   def new
-
+    @item = Item.new
   end
 
   def create
+    Item.create(item_params)
+  end
 
+  private
+  def item_params
+    params.require(:item).permit(:all)
   end
 
 end
