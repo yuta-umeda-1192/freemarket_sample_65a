1924a6d1bcd474b67766688d0d102bcc3702417b
AngelWingJasmineT <kterada0322@gmail.com>
2019-12-26 10:02:46 +0900

    deviseを使ったユーザー新規登録、ログイン（メールアドレスのみ）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :age, :family_name, :first_name, :family_name_kana, :first_name_kana ,:year ,:month ,:day])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana ,:year ,:month ,:day])
   end
 
 end
