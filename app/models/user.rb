497eafe9b32fb49abc9a9152dd50eba2055b5aac
AngelWingJasmineT <kterada0322@gmail.com>
2019-12-29 19:35:45 +0900

    【サーバサイド】ユーザー新規登録、ログイン(メールアドレスのみ)
    deviseのインストール
    userモデル作成
    ビューの嵌め込み
    単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana ,:year ,:month ,:day ,presence: true
+  validates :nickname, :email, :password, :family_name, :first_name, :family_name_kana, :first_name_kana, :year, :month, :day, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 7 }
   has_one :address
 end
