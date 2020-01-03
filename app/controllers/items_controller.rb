725ba726391167040ec6f46a2964a9e617b23136
AngelWingJasmineT <kterada0322@gmail.com>
2020-01-03 14:51:19 +0900

    item-list
    部分テンプレートでのビュー作成、TOPページへの差し込み、Itemテーブルとの紐付けまで完了（商品出品機能が未実装のため、imageテーブルとの連携ならびに単体テストは未実装。追って実装し、再レビュー予定）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.order('created_at DESC').page(params[:page]).per(5)
+  end
 
   def new
     @item = Item.new
@@ -6,6 +9,10 @@ class ItemsController < ApplicationController
 
   def create
     Item.create(item_params)
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
