bf3aa4a1bededfa2e7da60158add926d17ecefa6
GORI16 <ohnoyusuke16@icloude.com>
2020-01-07 15:47:04 +0900

    edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
+    if
+      @item.destroy
       redirect_to root_path
     else
       redirect_to edit_item_path
