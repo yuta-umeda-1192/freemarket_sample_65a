c75ecb95783c554f8344497d92b0fef16847a32b
AngelWingJasmineT <kterada0322@gmail.com>
2020-01-08 14:18:13 +0900

    toppage-header-syuppinbutton

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,11 @@
 class TopController < ApplicationController
+
   def index
     @items = Item.order('created_at DESC').page(params[:page]).per(5)
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
 end
